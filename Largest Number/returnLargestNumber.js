0d8e38440556d161fa495453fdce2c25a73f0c57
PhilomenaMusili <105357843+PhilomenaMusili@users.noreply.github.com>
2023-05-13 12:44:43 +0300

    Update returnLargestNumber.js

--- Largest Number/returnLargestNumber.js
+++ Largest Number/returnLargestNumber.js
@@ -1,1 +1,16 @@
+function largestOfFour(arr) {
+  const results = [];
+  for (let i = 0; i < arr.length; i++) {
+    let largestNumber = arr[i][0];
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+    results[i] = largestNumber;
+  }
 
+  return results;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
